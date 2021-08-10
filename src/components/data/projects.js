49b79099f18cc8f731f88671ce66aaabee423da7
Manish Bhamare <sushant.165.shinde@gmail.com>
2021-08-11 00:40:27 +0530

    Portfolio Dummy

---
+++ src/components/data/projects.js
@@ -0,0 +1,21 @@
+export const projectData = [
+    {
+        id : 1,
+        title : "Project One",
+        about : "lorem ipsum tanatana, lorem ipsum tanatana,",
+        tags : ["ReactJS", "NodeJS", "Firebase"],
+        demo : 'www.github.com/manishbb',
+        github : 'www.github.com/manishbb',
+        image : require("../../assets/coding.png").default,
+    },
+
+    {
+        id : 2,
+        title : "Project Two",
+        about : "lorem ipsum tanatana, lorem ipsum tanatana,",
+        tags : ["Kotlin", "TensarFlow", "MaterialUI", "AdSense", "Kotlin", "TensarFlow", "MaterialUI"],
+        demo : 'www.github.com/manishbb',
+        github : 'www.github.com/manishbb',
+        image : require('../../assets/coding.png').default,
+    },
+];
---
+++ src/components/data/social.js
@@ -0,0 +1,32 @@
+export const socialData = [
+    {
+        platform : "Instagram",
+        link : "www.instagram.com/desireddreamer",
+        icon : require("../../assets/instagram.png").default,
+    },
+
+    {
+        platform : "Facebook",
+        link : "www.instagram.com/desireddreamer",
+        icon : require("../../assets/facebook.png").default,
+    },
+
+    {
+
+        platform : "LinkedIn",
+        link : "www.instagram.com/desireddreamer",
+        icon : require("../../assets/linkedin.png").default,
+    },
+
+    {
+        platform : "GitHub",
+        link : "www.instagram.com/desireddreamer",
+        icon : require("../../assets/github.png").default,
+    },
+
+    {
+        platform : "Twitter",
+        link : "www.instagram.com/desireddreamer",
+        icon : require("../../assets/twitter.png").default,
+    },
+];
