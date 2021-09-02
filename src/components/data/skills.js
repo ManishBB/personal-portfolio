749f8bbedd52444366f20d6f7648af05df4a36dd
Manish Bhamare <mbb7224@gmail.com>
2021-09-02 13:27:07 +0530

    From VSCode setup

---
+++ src/components/data/skills.js
@@ -0,0 +1,76 @@
+export const skillsData = [
+    {
+        type : "Frontend",
+        list : [
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            },
+
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            },
+
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            },
+
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            }
+        ]
+    },
+
+    {
+        type : "Backend",
+        list : [
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            },
+
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            },
+
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            },
+
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            }
+        ]
+    },
+
+    {
+        type : "DevOps",
+        list : [
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            },
+
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            },
+
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            },
+
+            {
+                name : "ReactJS",
+                icon : <i class="devicon-react-original colored"></i>
+            }
+        ]
+    },
+];
---
+++ src/components/data/work.js
@@ -0,0 +1,28 @@
+export const WorkData = [
+    {
+        company : "Google",
+        designantion : "Software Engineer",
+        dateJoining : "02-02-2021",
+        dateEnd : "Present",
+        companyLogo : require("../../assets/instagram.png").default ,
+        work : "db fbdb sbfd sfb sdfb dsfb  bfsbs dfbdfb dfb",
+    },
+
+    {
+        company : "Google",
+        designantion : "Software Engineer",
+        dateJoining : "02-02-2021",
+        dateEnd : "Present",
+        companyLogo : require("../../assets/instagram.png").default ,
+        work : "db fbdb sbfd sfb sdfb dsfb  bfsbs dfbdfb dfb",
+    },
+
+    {
+        company : "Google",
+        designantion : "Software Engineer",
+        dateJoining : "02-02-2021",
+        dateEnd : "Present",
+        companyLogo : require("../../assets/instagram.png").default ,
+        work : "db fbdb sbfd sfb sdfb dsfb  bfsbs dfbdfb dfb",
+    },
+]
