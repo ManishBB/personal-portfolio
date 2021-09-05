3f3ab0610cb9f0e8364b573abff37023435f03fc
Manish Bhamare <mbb7224@gmail.com>
2021-09-05 15:06:16 +0530

    Updated work experience

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -1,20 +1,11 @@
 export const WorkData = [
     {
         company : "Wunderkinds Technologies & Systems Pvt Ltd",
-        designantion : "Android Developer",
+        designantion : "Android Developer Intern",
         dateJoining : "01/05/2019",
         dateEnd : "30/06/2019",
         companyLogo : require("../../assets/wktechsys.png").default ,
         work : ["Working on Basics of Android", "Bug fixes on a Institute Management App", "Worked on IOT based app on Android & Firebase"],
-    },
-
-    {
-        company : "The Spark Foundation",
-        designantion : "Web Developer",
-        dateJoining : "01-09-2021",
-        dateEnd : "Present",
-        companyLogo : require("../../assets/sparkfoundation.png").default ,
-        work : ["Working on MERN Stack (MongoDB, ExpressJS, ReactJS, NodeJS)", "Fetching data with built-in & custom API's", "Bulding JWT & WebRTC based Applications"],
-    },
+    }
 
 ]
