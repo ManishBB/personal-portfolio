8d2c7f7b4eb9896b3a6835ad6b5539ea8d13547c
Manish Bhamare <mbb7224@gmail.com>
2021-09-20 23:04:37 +0530

    Updated iNotebook project & favicon

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,6 +1,17 @@
 export const projectData = [
+
     {
         id : 1,
+        title : "iNotebook",
+        about : "A Full stack web app for keeping notes. Built with MERN stack",
+        tags : ["ReactJS", "NodeJS" , "ExpressJS" , "MongoDB" , "React Redux", "API's", "Bootstrap 5.0"],
+        demo : 'https://github.com/ManishBB/iNoteBook-MERN',
+        github : 'https://github.com/ManishBB/iNoteBook-MERN',
+        image : require("../../assets/iNotebook.png").default,
+    },
+
+    {
+        id : 2,
         title : "NewsMonkey",
         about : "A news platform which loads top headlines based on your favourite categories!",
         tags : ["ReactJS", "API's", "Bootstrap 5.0"],
@@ -10,7 +21,7 @@ export const projectData = [
     },
 
     {
-        id : 2,
+        id : 3,
         title : "TextUtils",
         about : "Text utility website, manipulate your text",
         tags : ["ReactJS"],
@@ -20,7 +31,7 @@ export const projectData = [
     },
 
     {
-        id : 2,
+        id : 4,
         title : "Sandwitch Club",
         about : "An android ap lets you order food in a cafe just by sitting on any table.",
         tags : ["Java", "XML", "Firebase"],
